3fee589c655594f0cb2a0cec1872e1690e6b9f39
Suraj Jadhav <suraj.jadhav@inin.com>
2016-02-27 16:03:58 +0530

    Added angular & other dependency. Bootstrapped new angular app too.

--- js/app/index.js
+++ js/app/index.js
@@ -1,7 +1,19 @@
 //my/shirt.js now does setup work
 //before returning its module definition.
-define(['ng'], function (angular) {
-    //Do setup work here
-    var app = {};
-    return app;
+define(['angular', 'angular-route'], function (ng, ngRoute) {
+	//Do setup work here
+	var app = ng.module('app', ['ngRoute']);
+	app.start = function () {
+		ng.bootstrap(document, ['app']);
+	};
+	app.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: '../../partials/home/index.html'
+		}).when('/dashboard', {
+			templateUrl: '../../partials/dashboard/index.html'
+		}).when('/settings', {
+			templateUrl: '../../partials/settings/index.html'
+		});
+	}]);
+	return app;
 });
--- js/app/main.js
+++ js/app/main.js
@@ -1,12 +1,22 @@
 require.config({
-    paths: {
-        ng: '../../bower_components/angular/angular.min',
-    }
+	shim: {
+		'angular': {
+            		exports: 'angular'
+		},
+		'angular-route': {
+			deps: ['angular']
+		}
+	},
+	paths: {
+		'angular': '../../bower_components/angular/angular.min',
+		'angular-route': '../../bower_components/angular-route/angular-route.min'
+	}
 });
 
 // This code might be called before the require.config() in main.js
 // has executed. When that happens, require.js will attempt to
 // load 'scripts/foo.js' instead of 'scripts/libs/foo-1.1.3.js'
 require(['index'], function(app) {
-	return {};
+	app.start();
+	return app;
 });
